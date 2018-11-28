e1b9646edb1274fbf8dec8273d6b0d572f1740b6
Mithun Technologies <devopstrainingblr@gmail.com>
2018-11-28 17:11:44 +0530

    Commited using maven

---
+++ src/main/java/com/mt/sample/HelloWorld.java
@@ -0,0 +1,27 @@
+package com.mt.sample;
+
+/**
+ * Hello world!
+ *
+ */
+import java.util.Date;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Welcome to Maven Build Tool !! Today Date is:  " + getLocalCurrentDate());
+
+	}
+
+	private static Date getLocalCurrentDate() {
+		String m = "Mithun";
+
+		return new Date();
+
+	}
+
+	private static Date getLocalCurrentDate1() {
+		return new Date();
+	}
+
+}
