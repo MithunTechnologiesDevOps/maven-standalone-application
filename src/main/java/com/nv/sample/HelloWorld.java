02c6ca777ca741513dbe4c66f1e4666a8abe7377
nvdevops <nvdevopslearningzone@gmail.com>
2022-12-20 15:04:36 +0530

    updated

--- src/main/java/com/nv/sample/HelloWorld.java
+++ src/main/java/com/nv/sample/HelloWorld.java
@@ -9,12 +9,12 @@ import java.util.Date;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Welcome to Maven Build Tool !! Today Date is:  " + getLocalCurrentDate());
+		System.out.println("Hello Welcome to NV Technologies!! Today Date is:  " + getLocalCurrentDate());
 
 	}
 
 	private static Date getLocalCurrentDate() {
-		String m = "Mithun Technologies";
+		String m = "NV Technologies";
 		return new Date();
 
 	}
